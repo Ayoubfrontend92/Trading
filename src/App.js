f4ff03e04fa3d290a87864f645088c41c2646f43
ayoub rezaei <ayoub.frontend1992@gmail.com>
2022-07-22 10:29:49 -0700

    completed subscribe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import About from './components/about/About';
+import Developer from './components/developer/Developer';
 import Hero from './components/hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import Subscribe from './components/subscribe/Subscribe';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <About/>
+      <Developer/>
+      <Subscribe/>
     </div>
   );
 }
